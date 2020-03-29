f660365bc5cdee6bf03678fa850adcfe62a6ed31
MdShihab <shihabshahriar@iut-dhaka.edu>
2020-03-29 21:54:09 +0600

    books create/index/new

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index.js')
 const authorRouter = require('./routes/authors.js')
+const bookRouter = require('./routes/books.js')
 
 
 app.set('view engine', 'ejs')
@@ -24,5 +25,6 @@ db.on('error', error => console.log(error))
 db.once('open',() => console.log('Connected to Mongoose'))
 app.use('/', indexRouter)
 app.use('/authors',authorRouter)
+app.use('/books',bookRouter)
 
 app.listen(process.env.PORT || 3000)
